79d02aa6694d6c00554b694a68870943ebb304f1
Ivanov Artem <ivard_iv@mail.ru>
2022-10-15 23:36:09 +0700

    Navigation click update

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 //var now = Date();
 //alert(now);
+function navBarVisibility() {
+  document.getElementById("navigation").style.visibility = "visible"
+  document.getElementById("navigationBars").style.visibility = "visible"
+  document.getElementById("navigationBars").style.borderRight = "0.1px grey solid"
+}
